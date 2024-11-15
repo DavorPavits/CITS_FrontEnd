6dca587187e0b05d195eca3d4e26d3b22ea76238
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-11-15 09:10:24 +0100

    First Excersize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Sum(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>1+2 equals {1+2}</p>
+  );
+}
+
+//Excersize 1
+function Greet({user}){
+  
+  if(!user){
+    return(<p>No user</p>);
+  }else if(user === "Bob"){
+    return(<p>Acces Denied</p>);
+  }else{
+  return(
+    <h1>Hello {user}</h1>
+  );
+  }
+}
+
+function App(){
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Made with React.</p>
+      <Sum/>
+      {/* <Greet user={"Bob"}/> */}
+      <Greet user="Davor"/>
     </div>
   );
 }
